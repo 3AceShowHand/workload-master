f0476f4af97d452f5d43132ccf6682c8a187735a
3AceShowHand <jinl1037@hotmail.com>
2021-09-12 01:46:05 +0800

    add workload interface.

--- cmd/coverage.go
+++ cmd/coverage.go
@@ -1,6 +1,12 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
 
 func RegisterSQLCoverage(root *cobra.Command) {
 	cmd := &cobra.Command{
@@ -14,4 +20,39 @@ func RegisterSQLCoverage(root *cobra.Command) {
 
 func runSQLCoverage() {
 
+}
+
+var (
+	sqlFormat = map[string]string{
+		"insert":  "",
+		"delete":  "",
+		"update":  "",
+		"replace": "",
+	}
+)
+
+type SQLCoverageTask struct {
+	db      *sql.DB
+	dbName  string
+	threads int
+}
+
+func (t *SQLCoverageTask) Name() string {
+	return fmt.Sprintf("SQL Coverage Task")
+}
+
+func (t *SQLCoverageTask) CleanUp(ctx context.Context, threadID int) error {
+	return nil
+}
+
+func (t *SQLCoverageTask) Prepare(ctx context.Context, threadID int) error {
+	return nil
+}
+
+func (t *SQLCoverageTask) Run(ctx context.Context, threadID int) error {
+	return nil
+}
+
+func (t *SQLCoverageTask) DBName() string {
+	return t.dbName
 }
---
+++ pkg/workload/task.go
@@ -0,0 +1,11 @@
+package workload
+
+import "context"
+
+type Task interface {
+	Name() string
+	CleanUp(ctx context.Context, threadID int) error
+	Prepare(ctx context.Context, threadID int) error
+	Run(ctx context.Context, threadID int) error
+	DBName() string
+}
